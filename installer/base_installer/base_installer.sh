97248af17ed43b961dbf0b8e93a52c3defedb0d2
thekeymaker <bkmiller15@gmail.com>
2015-08-03 17:14:28 -0500

    Added swap and corrected some more items in script.  Still need to get grub working

--- installer/base_installer/base_installer.sh
+++ installer/base_installer/base_installer.sh
@@ -20,23 +20,27 @@ HARDDRIVE_PATH=$(zenity --file-selection)
 echo $HARDDRIVE_PATH
 
 
-apt-get install --yes mbr
 
 # Install Needed ZFS tools
 apt-add-repository --yes ppa:zfs-native/stable
 apt-get update
-apt-get install --yes debootstrap spl-dkms zfs-dkms ubuntu-zfs
+apt-get install --yes debootstrap ubuntu-zfs
 
 
 # Format HD
 #Prompt would be nice
+# Add swap/boot/ and root
 echo "Formating HD"
-install-mbr $HARDDRIVE_PATH
-(echo n; echo p; echo 1; echo; echo +256M; echo n; echo p; echo 2; echo; echo; echo a; echo 1; echo p; echo w) | fdisk $HARDDRIVE_PATH
+(echo g; echo n; echo 1; echo; echo +2G; echo n; echo 2; echo; echo +256M; echo n; echo 3; echo; echo; echo t; echo 1; echo 14; echo t; echo 2 echo 4; echo p; echo w) | fdisk $HARDDRIVE_PATH
 
-mke2fs -m 0 -L /boot/grub -j ${HARDDRIVE_PATH}-part1
+mkswap -L swap ${HARDDRIVE_PATH}-part1
+mkfs.ext3 ${HARDDRIVE_PATH}-part2
 
-zpool create -o ashift=9 $POOL_NAME ${HARDDRIVE_PATH}-part2
+
+#Label disks
+
+zpool create -d -o feature@async_destroy=enabled -o feature@empty_bpobj=enabled -o feature@lz4_compress=enabled -o ashift=12 -O compression=lz4 rpool /dev/sda3
+# zpool export rpool
 
 zfs create ${POOL_NAME}/ROOT
 zfs create ${POOL_NAME}/ROOT/zed-1
@@ -44,14 +48,13 @@ zfs create ${POOL_NAME}/ROOT/zed-1
 zfs umount -a
 
 zfs set mountpoint=/ ${POOL_NAME}/ROOT/zed-1
-zfs set bootfs=${POOL_NAME}/ROOT/zed-1 $POOL_NAME
-
-zpool export $POOL_NAME
+zpool set bootfs=${POOL_NAME}/ROOT/zed-1 $POOL_NAM export $POOL_NAME
+	
 
 zpool import -d /dev/disk/by-id -R /mnt $POOL_NAME
 
 mkdir -p /mnt/boot/grub
-mount ${HARDDRIVE_PATH}-part1 /mnt/boot/grub
+mount ${HARDDRIVE_PATH}-part2 /mnt/boot/grub
 
 debootstrap trusty /mnt
 
@@ -64,6 +67,9 @@ echo "${HARDDRIVE_PATH}-part1  /boot/grub  auto  defaults  0  1" >> /mnt/etc/fst
 mount --bind /dev  /mnt/dev
 mount --bind /proc /mnt/proc
 mount --bind /sys  /mnt/sys
+
+#cat /etc/modprobe.d/zfs-arc-max.conf 
+#options zfs zfs_arc_max=1073741824
 
 #chroot /mnt /bin/bash --login
 
