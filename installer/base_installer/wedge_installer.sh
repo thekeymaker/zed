7eb4017e28aec03310de89efbb9bc26422f4cc27
thekeymaker <bkmiller15@gmail.com>
2015-08-17 17:05:41 -0500

    Added yes to wedge script so you don't get prompted

--- installer/base_installer/wedge_installer.sh
+++ installer/base_installer/wedge_installer.sh
@@ -3,15 +3,15 @@
 # Setup apt-get
 locale-gen en_US.UTF-8
 apt-get update
-apt-get install ubuntu-minimal software-properties-common
+apt-get install --yes ubuntu-minimal software-properties-common
 
 apt-add-repository --yes ppa:zfs-native/stable
 apt-get update
-apt-get install --no-install-recommends linux-image-generic linux-headers-generic
-apt-get install ubuntu-zfs
-apt-get install grub2-common grub-pc
-apt-get install zfs-initramfs
-apt-get dist-upgrade
+apt-get install --yes --no-install-recommends linux-image-generic linux-headers-generic
+apt-get install --yes ubuntu-zfs
+apt-get install --yes grub2-common grub-pc
+apt-get install --yes zfs-initramfs
+apt-get --yes dist-upgrade
 
 passwd root
 
