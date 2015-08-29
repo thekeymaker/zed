7ff0381e975c46916eae875e23bd78e0fb698db6
thekeymaker <unreal.energy@gmail.com>
2015-08-29 12:12:36 -0500

    Set vim script to load proper font and also updated settings

--- installer/desktop_installer/vim.sh
+++ installer/desktop_installer/vim.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# Install cmake - needed for ycm plugin
+mkdir ~/.fonts
+
+# Install cmake and python-dev - needed for ycm plugin
 sudo apt-get install cmake
 sudo apt-get install python-dev
 
@@ -14,3 +16,10 @@ git submodule update --init --recursive
 cd bundle/youcompleteme
 
 ./install.sh
+
+# Install needed fonts
+cd ~/.vim/fonts/Meslo
+cp *.otf ~/.fonts/
+
+# Set font settings for dconf
+gsettings set org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9 font 'Meslo LG L DZ for Powerline 12'
