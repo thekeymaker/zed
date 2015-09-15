950643e8157e8b4538028e3ea746e277f46df64d
thekeymaker <unreal.energy@gmail.com>
2015-09-15 07:58:46 -0500

    Added rebase command

--- installer/desktop_installer/git.sh
+++ installer/desktop_installer/git.sh
@@ -16,6 +16,7 @@ git config --global core.editor vi
 git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
+git config --global alias.rb rebase
 git config --global alias.st status
 
 git config --global alias.lg  "log --graph --decorate" 
