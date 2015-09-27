48014833151539a47fef9ef714fe1cc700aa9146
thekeymaker <unreal.energy@gmail.com>
2015-09-27 15:59:01 -0500

    Added usage description to autosnap script

--- installer/desktop_installer/resources/autosnap.sh
+++ installer/desktop_installer/resources/autosnap.sh
@@ -1,7 +1,30 @@
 #!/bin/bash
-
-set -x
  
+# Function to display usage:
+usage() {
+    scriptname=`/usr/bin/basename $0`
+    echo "$scriptname: Take and rotate snapshots on a ZFS file system"
+    echo
+    echo "  Usage:"
+    echo "  $scriptname target snap_name count"
+    echo
+    echo "  target:    ZFS file system to act on"
+    echo "  snap_name: Base part of name used to help define snapshot"
+    echo "  count:     Number of snapshots in the snap_name.number format to"
+    echo "             keep at one time.  Newest snapshot ends in '.0'."
+    echo
+    exit
+}
+                                                    
+# Basic argument checks:
+if [ -z $COUNT ] ; then
+	usage
+fi
+                                                          
+if [ ! -z $4 ] ; then
+    usage
+fi
+
 # Path to ZFS executable:
 ZFS=/sbin/zfs
  
@@ -12,15 +35,15 @@ COUNT=$3
 MAXSNAP=$(($COUNT -1))
 
 # First delete the oldest backup
-$ZFS destroy -r ${TARGET}@autobak.${SNAPTYPE}.${MAXSNAP}
+$ZFS destroy -r ${TARGET}@autosnap.${SNAPTYPE}.${MAXSNAP}
  
 # Rename existing snapshots:
 INDEX=$(($MAXSNAP -1))
 while [ $INDEX -ge 0 ] ; do
 	NEWNUM=$(($INDEX +1))
-    $ZFS rename -r ${TARGET}@autobak.${SNAPTYPE}.${INDEX} ${TARGET}@autobak.${SNAPTYPE}.${NEWNUM}
+    $ZFS rename -r ${TARGET}@autosnap.${SNAPTYPE}.${INDEX} ${TARGET}@autosnap.${SNAPTYPE}.${NEWNUM}
 	INDEX=$(($INDEX -1))
 done
  
 # Create new snapshot:
-$ZFS snapshot -r ${TARGET}@autobak.${SNAPTYPE}.0
+$ZFS snapshot -r ${TARGET}@autosnap.${SNAPTYPE}.0
