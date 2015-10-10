85300d7967f3ec08700d1e0ecd9f968964da5ea6
thekeymaker <unreal.energy@gmail.com>
2015-10-10 09:11:15 -0500

    Fixed nfs script for client

--- installer/desktop_installer/scripts/nfs.sh
+++ installer/desktop_installer/scripts/nfs.sh
@@ -17,12 +17,12 @@ fi
 if [ $ENTRY == "Client" ]; then
 	echo "Setup Client"
 	sudo apt-get install nfs-common
-	addgroup nfs --gid 1500
-	usermod -a -G nfs $USER
+	sudo addgroup nfs --gid 1500
+	sudo usermod -a -G nfs $USER
 
 	sudo apt-get install autofs
-	sudo cp ./resources/auto.master /etc/*
-	sudo cp ./resources/auto.nfs /etc/*
+	sudo cp ./resources/auto.master /etc/
+	sudo cp ./resources/auto.nfs /etc/
 	sudo mkdir -p /mnt/nfs
 
 fi
