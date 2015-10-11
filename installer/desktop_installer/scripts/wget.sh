fa51b482f406c240fb563f669694b0956b09b592
thekeymaker <unreal.energy@gmail.com>
2015-10-11 09:58:50 -0500

    Removed atom as md editor and replaced it with remarkable Also added musescore

--- installer/desktop_installer/scripts/wget.sh
+++ installer/desktop_installer/scripts/wget.sh
@@ -8,9 +8,15 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb .
 sudo dpkg -i google-chrome*.deb
 
 # Install Atom
-wget https://atom.io/download/deb .
-mv deb atom.deb
-sudo dpkg -i ./atom.deb
+#wget https://atom.io/download/deb .
+#mv deb atom.deb
+#sudo dpkg -i ./atom.deb
+
+# Install Remarkable
+wget https://remarkableapp.github.io/files/remarkable_1.62_all.deb .
+mv remarkable* remarkable.deb
+sudo dpkg -i ./remarkable.deb
+
 
 # Install Steam
 wget https://steamcdn-a.akamaihd.net/client/installer/steam.deb .
