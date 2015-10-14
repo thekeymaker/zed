1c0ce580436d2979a2d4092de61e86c69229ee82
thekeymaker <unreal.energy@gmail.com>
2015-10-14 07:37:34 -0500

    Added more comments to base_installer script

--- installer/base_installer/base_installer.sh
+++ installer/base_installer/base_installer.sh
@@ -4,13 +4,8 @@
 # Enable debug mode
 set -x
 
-WD=`pwd`
-RELEASE="vivid"
-POOL_NAME=rpool
-SYSNAME="zed-1"
-CHROOTVAR+="${SYSNAME}|"
-echo $CHROOTVAR
-
+RELEASE="vivid"  # Version of Ubuntu to use
+POOL_NAME=rpool  # Pool name
 
 function check_exit_code()
 {
@@ -29,15 +24,16 @@ function check_if_user_is_root()
 	fi
 }
 
+WD=`pwd`
 
 WELCOME_TEXT=`cat <<EOF
 Welcome to Zed Base Installer!
 
 The following prompt will ask you to give some imformation
 about the machine name and users of this computer.  The user
-that is created will also me given sudo permissions. 
+that is created will also be given sudo permissions. 
 
-After that you will be asked which harddrive the installer 
+After that you will be asked which hard drive the installer 
 should install too.  
 
 Thanks againg for trying this install!
@@ -51,21 +47,35 @@ echo -n "$WELCOME_TEXT" | zenity --title "WELCOME" --text-info --width=500 --hei
 
 # Get user settings
 ENTRY=`zenity --forms --title="Install Info" --text="Please fill in all information below:" --add-entry="Hostname:" --add-password="Root Password:" --add-entry="Username:" --add-password="User Password:"`
+
+if [ -z $ENTRY]; then
+   echo "Failed to get user data"
+   exit
+fi
+
+# Saves vars in the format ( var1 | var2 | ... ) to be passed into chroot environment
 CHROOTVAR+="${ENTRY}|"
 
 # Set install hard drive
 cd /dev/disk/by-id
 HARDDRIVE_PATH=$(zenity --file-selection)
 
+if [ -z $HARDDRIVE_PATH]; then
+   echo "Failed to get hard drive"
+   exit
+fi
+
 
 # Install Needed ZFS tools
 apt-add-repository --yes ppa:zfs-native/stable
+check_exit_code
 apt-get update
 apt-get install --yes debootstrap ubuntu-zfs
 check_exit_code
 
 
-# Partistion HD with boot/swap/zfs 
+# Partition hard drive with boot/swap/zfs 
+echo
 echo "Formating HD"
 parted -a optimal ${HARDDRIVE_PATH} < ${WD}/partitions.txt
 sync
@@ -83,63 +93,62 @@ swapon ${HARDDRIVE_PATH}-part2
 zpool create -d -o feature@async_destroy=enabled -o feature@empty_bpobj=enabled -o feature@lz4_compress=enabled -o ashift=12 -O compression=lz4 $POOL_NAME ${HARDDRIVE_PATH}-part3
 
 zfs create ${POOL_NAME}/ROOT
-#zfs create ${POOL_NAME}/ROOT/$SYSNAME
 zfs create ${POOL_NAME}/BOOT
 zfs create ${POOL_NAME}/HOME
 
 zfs umount -a
 
-#zfs set mountpoint=/ ${POOL_NAME}/ROOT/$SYSNAME
+# Set mount points for new datasets
 zfs set mountpoint=/     ${POOL_NAME}/ROOT
 zfs set mountpoint=/boot ${POOL_NAME}/BOOT
 zfs set mountpoint=/home ${POOL_NAME}/HOME
-#zpool set bootfs=${POOL_NAME}/ROOT/$SYSNAME $POOL_NAME
 	
 zpool export $POOL_NAME
 
+# Import pool under /mnt
 zpool import -d /dev/disk/by-id -R /mnt $POOL_NAME
 
-# Load minimal ubunt file structure
+# Load minimal Ubuntu file structure
 debootstrap $RELEASE /mnt
 
 cp /etc/hostname /mnt/etc/
 cp /etc/hosts /mnt/etc/
 
+# Enable swap in fstab
 echo "${HARDDRIVE_PATH}-part2  none   swap  sw        0  0" >> /mnt/etc/fstab
 
+# Mount needed system directories
 mount --bind /dev  /mnt/dev
 mount --bind /proc /mnt/proc
 mount --bind /sys  /mnt/sys
 
+# Could be used to limit memory usage of zarc.  Not tested
 #cat /etc/modprobe.d/zfs-arc-max.conf 
 #options zfs zfs_arc_max=1073741824
 
-#Setup neede items for grub
+# Create link to hard drive so grub can find it and doesn't error out
 HARDDRIVE=`basename ${HARDDRIVE_PATH}`
 ln -s ${HARDDRIVE_PATH}-part3 /dev/${HARDDRIVE}-part3
 
-#Copy`
+# Copy over install files form base_chroot directory 
 cd $WD
 cp -r ./base_chroot /mnt
 
 # CHROOT!
+echo
 echo "Chroot!"
 chroot /mnt /bin/bash /base_chroot/wedge_installer.sh $CHROOTVAR
-#chroot /mnt /bin/bash --login
+#chroot /mnt /bin/bash --login # Here for testing
 
 # Remove wedge script
 rm -rf /mnt/base_chroot
-
-# Copy in this source file for later config
-#mkdir -p /mnt/home/${USERNAME}/scripts
-#cp -r ${PWD}/../desktop_installer /mnt/home/${USERNAME}/scripts
-
 
 # Create snapshot of system 
 zfs snapshot ${POOL_NAME}/ROOT@bInit
 zfs snapshot ${POOL_NAME}/BOOT@bInit
 zfs snapshot ${POOL_NAME}/HOME@bInit
 
+echo
 echo "Finished!"
 
 
